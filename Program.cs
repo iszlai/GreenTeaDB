a6184a9ec2c8b6acb71edf367fba3522c58517fc
iszlai <iszlai.lehel@gmail.com>
2014-10-04 14:53:51 +0200

    Starting out
    Initial commit starting with emulating functionality

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeaDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringDBOperation teaDB = StringDBOperation.getDBOperation();
+            teaDB.store("alma", "korte");
+            System.Console.WriteLine(teaDB.fetch("alma"));
+            System.Console.ReadKey();
+        }
+    }
+}
---
+++ io.cofeemon.teadb/StringDBOperation.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeaDB
+{
+    class StringDBOperation : DBOperation
+    {
+        private Dictionary<string, string> inMemoryStore;
+        private static volatile StringDBOperation instance = null;
+        private static object syncRoot = new Object();
+
+
+
+        private StringDBOperation()
+        {
+            inMemoryStore=new Dictionary<string,string>();
+        }
+
+        public static StringDBOperation getDBOperation()
+        {
+
+            if (instance == null)
+            {
+                lock (syncRoot)
+                {
+                    if (instance == null)
+                        instance = new StringDBOperation();
+                }
+            }
+
+
+            return instance;
+        }
+
+        public void store(string key, string value)
+        {
+            inMemoryStore.Add(StringDBOperation.hash(key), value);
+
+        }
+
+        public string fetch(string key)
+        {
+            var internalKey = StringDBOperation.hash(key);
+            if (inMemoryStore.ContainsKey(internalKey))
+            {
+                return inMemoryStore[internalKey];
+             }
+            return null;
+        }
+
+        public static string hash(string value)
+        {
+            int hashValue = 0;
+            foreach (char c in value)
+            {
+                hashValue = (hashValue << 5) - hashValue + c;
+            }
+            return hashValue.ToString();
+        }
+    }
+}
